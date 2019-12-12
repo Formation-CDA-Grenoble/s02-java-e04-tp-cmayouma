c87124194368bedc4c6e95d39bff437a0e9b4c53
cmayouma <58431935+cmayouma@users.noreply.github.com>
2019-12-12 14:14:59 +0100

    exercice 2 fini

--- src/app/Character.java
+++ src/app/Character.java
@@ -1,5 +1,7 @@
 package app;
 
+import javax.lang.model.util.ElementScanner6;
+
 class Character {
     
     int lives;
@@ -10,13 +12,27 @@ class Character {
 
     Character(int number){
         lives = number;
-
     }
 
-
-
-
-
+    private int getLives() {
+        return lives;
+    }
+    private  void addLife() {
+        lives++;      
+    }
+    private  void loseLife () {
+        lives--;        
+    }
+    private  boolean haslive () {
+        boolean result;
+        if(lives>0){
+            result = true;
+        }    else { 
+            result = false;
+        }
+        return result;
+        
+    }
 
 
 
